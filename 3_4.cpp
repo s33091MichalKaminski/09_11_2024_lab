6f0730448ce4829537c133ee7e789fa917783c5d
s33091MichalKaminski <michal.kaminski232@gmail.com>
2024-11-09 16:40:16 +0100

    Update 3_4.cpp

--- 3_4.cpp
+++ 3_4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int w;
+	cout << "podaj liczbe wierszy trojkata Pascala" << endl;
+	cin >> w;
+	cout << endl;
+
+	for(int i = 0; i < w; i++) {
+		int poczatek = 1;
+		for (int j = 0; j <= i; j++) {
+			cout << poczatek << " ";
+			poczatek = poczatek * (i - j) / (j + 1);
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
 
